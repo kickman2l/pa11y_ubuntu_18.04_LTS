ec21cad71c6ac72886b4815e126c40ed9120ccff
Pavel Heraska <pheraska@exadel.com>
2020-12-14 15:59:19 +0300

    fasfdasdf

--- install_pa11y.sh
+++ install_pa11y.sh
@@ -16,15 +16,12 @@ sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 sudo /bin/cp -r ./default /etc/nginx/sites-available/
 sudo /bin/cp -r ./self-signed.conf /etc/nginx/snippets/
 sudo /bin/cp -r ./ssl-params.conf /etc/nginx/snippets/
-sudo /bin/cp -r ./pa11y.service /etc/systemd/system/
 
 # checkout and install pa11y
 git clone https://github.com/pa11y/pa11y-dashboard.git
 cd ./pa11y-dashboard/ && npm install
 
-sudo systemctl enable pa11y
-sudo systemctl start pa11y
-
 #start nginx
 sudo nginx -s reload
 sudo systemctl start nginx
+forever start -al /home/ubuntu/forever.log -ao /home/ubuntu/out.log -ae /home/ubuntu/err.log /home/ubuntu/pa11y_ubuntu_18.04_LTS/pa11y-dashboard/index.js
