6d87c639735936f89663726fe0ff0094c03fb068
Pavel Heraska <pheraska@exadel.com>
2020-12-14 14:02:55 +0300

    qwedasdasdasd

--- install_pa11y.sh
+++ install_pa11y.sh
@@ -15,15 +15,15 @@ sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
 sudo /bin/cp -r ./default /etc/nginx/sites-available/
 sudo /bin/cp -r ./self-signed.conf /etc/nginx/snippets/
 sudo /bin/cp -r ./ssl-params.conf /etc/nginx/snippets/
+sudo /bin/cp -r ./pa11y.service /etc/systemd/system/
 
 # checkout and install pa11y
 git clone https://github.com/pa11y/pa11y-dashboard.git
 cd ./pa11y-dashboard/ && npm install
 
+sudo systemctl enable pa11y
+sudo systemctl start pa11y
+
 #start nginx
 sudo nginx -s reload
 sudo systemctl start nginx
-
-sudo /bin/cp -r ./pa11y.service /etc/systemd/system/
-sudo systemctl enable pa11y
-sudo systemctl start pa11y
